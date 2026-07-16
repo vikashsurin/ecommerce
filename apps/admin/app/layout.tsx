f47532f823a33ce8787dba80152a2db7466c2393
VikashSurin <vikashsurin10@gmail.com>
2026-07-16 14:34:05 +0530

    removed sonner

--- apps/admin/app/layout.tsx
+++ apps/admin/app/layout.tsx
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import './globals.css'
 
-import { Toaster } from "@workspace/ui/components/sonner"
+import { Toaster } from "sonner"
 
 import "@workspace/ui/globals.css";
 // import { ThemeProvider } from "@/components/theme-provider"
